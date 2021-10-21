823ea8c3f61f24b47bc14622642c08e549317d8b
reigending <yangyan@yangyandeMacBook-Pro.local>
2021-10-21 23:13:40 +0800

    bubble sort

--- algorithm/algorithm/01、a,b,c_combine.py
+++ algorithm/algorithm/01、a,b,c_combine.py
@@ -5,13 +5,32 @@ def solution():
     for a in range(1001):
         for b in range(1001):
             for c in range(1001):
-                if a+b+c ==1000 and a**2 +b**2 == c**2:
-                    print("a: %d, b: %d, c: %d" % (a, b, c)) 
+                if a+b+c ==1000 and a**2 + b**2 == c**2:
+                    print("a: %d, b: %d, c: %d" % (a, b, c))
+
+
+def solution2():
+    for a in range(1001):
+        for b in range(1001):
+            c = 1000 -a -b
+            if a**2 + b**2 == c**2:
+                print("a: %d, b: %d, c: %d" % (a, b, c))
+
+
+
+# T(n) = k *f(n) +c 
+# f(n) = n * n 
+# f(n)叫做T(n)的渐进函数 
+# solution的时间复杂度就是O(n^3) 
+# solutiosn2的时间复杂度就是O(n^2) #
+
+
 
 
 if __name__ == "__main__":
     start_time = time.time()
-    solution()
+    # solution()    # 109s
+    solution2()     # 1.6s
     end_time = time.time()
     print("花费时间： ", end_time-start_time)
 
---
+++ algorithm/algorithm/02、sort.py
@@ -0,0 +1,44 @@
+# Bubble sort冒泡排序
+
+from typing import Mapping
+
+
+def bubble_sort(alist):
+    # 相邻连个元素进行比较，如果发现位置错误则进行交换位置
+    print(id(alist))
+    length = len(alist)
+    for k in range(length):
+        for i in range(length-1-k):
+            if alist[i] > alist[i+1]:
+                alist[i], alist[i+1] = alist[i+1], alist[i]
+        print(alist)
+    print(alist)
+
+
+# 如果传入的列表就是有序，只需要检测一轮，如果没有进行交换，则直接退出循环
+def bubble_sort2(alist):
+    # 相邻连个元素进行比较，如果发现位置错误则进行交换位置
+    print(id(alist))
+    length = len(alist)
+    for k in range(length-1):
+        count = 0
+        for i in range(length-1-k):
+            if alist[i] > alist[i+1]:
+                count += 1
+                alist[i], alist[i+1] = alist[i+1], alist[i]
+        print(alist)
+        if count == 0:
+            break
+    print(alist)
+
+
+if __name__ == "__main__":
+    alist = [7 ,4, 6, 9, 1]
+    alist2 = [1, 2, 3, 4, 5]
+    alist3 = [1, 2, 4, 3, 5]
+    # print(id(alist))
+    # bubble_sort(alist)
+    # print(alist)
+    bubble_sort2(alist)
+    bubble_sort2(alist2)
+    bubble_sort2(alist3)
